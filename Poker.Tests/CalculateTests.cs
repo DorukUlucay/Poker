4ed9e966271ba6947957cf6b6d60a20e05385379
Doruk.Ulucay <doruk.ulucay@cronom.com>
2015-04-25 18:44:02 +0300

    initial commit

---
+++ Poker.Tests/CalculateTests.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Poker;
+
+namespace Poker.Tests
+{
+    [TestClass]
+    public class CalculateTests
+    {
+        [TestMethod]
+        public void IsFlush_Flush_ReturnTrue()
+        {
+            Poker.Calculator Calc = new Calculator();
+
+            string[] x = { "S2", "S4", "S6", "SQ", "SJ" };
+            bool y = Calc.isFlush(x);
+
+            Assert.AreEqual(true, y);
+        }
+
+        [TestMethod]
+        public void IsFlush_NotFlush_ReturnFalse()
+        {
+            Poker.Calculator Calc = new Calculator();
+
+            string[] x = { "S2", "HA", "SQ", "SQ", "SJ" };
+            bool y = Calc.isFlush(x);
+
+            Assert.AreEqual(false, y);
+        }
+
+        [TestMethod]
+        public void IsStraight_Straight_ReturnTrue()
+        {
+            Poker.Calculator Calc = new Calculator();
+
+            int[] x = { 1, 2, 3, 4, 5 };
+            bool y = Calc.isStraight(x);
+
+            Assert.AreEqual(true, y);
+        }
+
+        [TestMethod]
+        public void IsFullHouse_FullHouse_ReturnTrue()
+        {
+            Poker.Calculator Calc = new Calculator();
+
+            int[] x = { 2, 3, 2, 2, 3 };
+            bool y = Calc.isFullHouse(x);
+
+            Assert.AreEqual(true, y);
+        }
+
+        [TestMethod]
+        public void IsFullHouse_NotFullHouse_ReturnFalse()
+        {
+            Poker.Calculator Calc = new Calculator();
+
+            int[] x = { 2, 3, 4, 2, 3 };
+            bool y = Calc.isFullHouse(x);
+
+            Assert.AreEqual(false, y);
+        }
+
+        [TestMethod]
+        public void IsThreeOfAKind_GivenThreeOfAKind_ReturnTrue()
+        {
+            Poker.Calculator Calc = new Calculator();
+
+            int[] x = { 5, 6, 7, 6, 6 };
+            bool y = Calc.isThreeOfAKind(x);
+
+            Assert.AreEqual(true, y);
+        }
+
+        [TestMethod]
+        public void IsThreeOfAKind_NotGivenThreeOfAKind_ReturnFalse()
+        {
+            Poker.Calculator Calc = new Calculator();
+
+            int[] x = { 5, 6, 7, 8, 6 };
+            bool y = Calc.isThreeOfAKind(x);
+
+            Assert.AreEqual(false, y);
+        }
+
+        [TestMethod]
+        public void IsPair_GivenPair_ExpectedTrue()
+        {
+            Poker.Calculator Calc = new Calculator();
+
+            string[] Hands = { "S9S8H7H8CA" };
+            Poker.Calculator.CalculatedHand C = Calc.CalculateHand(Hands[0]);
+
+            Assert.AreEqual(Poker.Calculator.HandRanks.Pair, C.Rank);
+        }
+
+        [TestMethod]
+        public void IsFour_GivenFour_ExpectedTrue()
+        {
+            Poker.Calculator Calc = new Calculator();
+
+            int[] sorted = { 2, 2, 2, 2, 1 };
+            bool result = Calc.isFourOfAKind(sorted);
+
+            Assert.AreEqual(true, result);
+        }
+
+
+        
+        [TestMethod]
+        public void CalculateHand_GivenRoyalFlush_ExpectedTrue()
+        {
+            Poker.Calculator Calc = new Calculator();
+
+            string[] Hands = { "HKHQH0HJHA" };
+            Poker.Calculator.CalculatedHand C = Calc.CalculateHand(Hands[0]);
+
+            Assert.AreEqual(Poker.Calculator.HandRanks.RoyalFlush, C.Rank);
+        }
+
+        [TestMethod]
+        public void CalculateHand_Given8HighStraightFlush_Expected8HighStraightFlush()
+        {
+            Poker.Calculator Calc = new Calculator();
+
+            string[] Hands = { "H7H5H6H8H4" };
+            Poker.Calculator.CalculatedHand C = Calc.CalculateHand(Hands[0]);
+
+            Assert.AreEqual(Poker.Calculator.HandRanks.StraightFlush, C.Rank);
+            Assert.AreEqual(8, C.HighCard);
+        }
+
+        [TestMethod]
+        public void CalculateHand_GivenJHigh4OfAKind_ExpectedJHighFourOfAKind()
+        {
+            Poker.Calculator Calc = new Calculator();
+
+            string[] Hands = { "HJHKSJCJDJ" };
+            Poker.Calculator.CalculatedHand C = Calc.CalculateHand(Hands[0]);
+
+            Assert.AreEqual(Poker.Calculator.HandRanks.FourOfAKind, C.Rank);
+            Assert.AreEqual(11, C.HighCard);
+        }
+    }
+}
---
+++ Poker/Calculate.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker
+{
+    public class Calculator
+    {
+
+        public enum HandRanks
+        {
+            RoyalFlush, 
+            StraightFlush, 
+            FourOfAKind, 
+            FullHouse, 
+            Flush, 
+            Straight, 
+            Three, 
+            TwoPair, 
+            Pair, 
+            HighCard
+        }
+
+        public class CalculatedHand
+        {
+            public HandRanks Rank;
+            public int HighCard;
+        }
+
+        //hands are a string array. it'll be json
+        public void CalculateHands(string[] Hands)
+        {
+            foreach (string Hand in Hands)
+            {
+                CalculateHand(Hand);
+            }
+        }
+
+
+        public CalculatedHand CalculateHand(string Hand)
+        {
+            string[] Cards =  Split(Hand, 2).ToArray();
+
+            bool flush = isFlush(Cards);
+
+            int[] nums = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                string Number = Cards[i][1].ToString();
+
+                int val = 0;
+
+                if (!int.TryParse(Number, out val))
+                {
+                    switch (Number)
+                    {
+                        case "A": val = 14; break;
+                        case "K": val = 13; break;
+                        case "Q": val = 12; break;
+                        case "J": val = 11; break;
+                    }
+                }
+                nums[i] = val == 0 ? 10 : val;
+            }
+
+            Array.Sort(nums);
+
+            bool Straight = isStraight(nums);
+            int HighCard = nums[4];
+
+            CalculatedHand CH = new CalculatedHand();
+            CH.HighCard = HighCard;
+
+            if (flush & Straight)
+            {
+                if (HighCard == 14)
+                {
+                    CH.Rank = HandRanks.RoyalFlush;    
+                }
+                else
+                {
+                    CH.Rank = HandRanks.StraightFlush;
+                }
+            }
+            else if (isFourOfAKind(nums))
+            {
+                CH.Rank = HandRanks.FourOfAKind;
+            }
+            else if (isFullHouse(nums))
+            {
+                CH.Rank = HandRanks.FullHouse;
+            }
+            else if (flush)
+            {
+                CH.Rank = HandRanks.Flush;
+            }
+            else if (Straight)
+            {
+                CH.Rank = HandRanks.Straight;
+            }
+            else if (isThreeOfAKind(nums))
+            {
+                CH.Rank = HandRanks.Three;
+            }
+            else if (isTwoPair(nums))
+            {
+                CH.Rank = HandRanks.TwoPair;
+            }
+            else if (IsPair(nums))
+            {
+                CH.Rank = HandRanks.Pair;
+            }
+            else
+            {
+                CH.Rank = HandRanks.HighCard;
+            }
+
+            return CH;
+        }
+
+
+
+        public bool isFlush(string[] Cards)
+        {
+            char suite = 'N';
+            bool isFlush = true;
+
+            suite = Cards[0][0];
+
+            foreach (string card in Cards)
+            {
+                if (suite != card[0])
+                {
+                    isFlush = false;
+                }
+            }
+
+            return isFlush;
+        }
+
+
+        public bool isStraight(int[] sorted)
+        {
+            if (sorted[4]-1 == sorted[3]
+                && sorted[3] - 1 == sorted[2]
+                && sorted[2] - 1 == sorted[1]
+                && sorted[1] - 1 == sorted[0]
+                )
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool isFourOfAKind(int[] sorted)
+        {
+            var x = sorted.GroupBy(o => o);
+
+            if (x.Count() == 2)
+            {
+                if (x.ElementAt(0).Count() == 4 || x.ElementAt(1).Count() == 4)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+        public bool isFullHouse(int[] sorted)
+        {
+            var x = sorted.GroupBy(o => o);
+
+            if (x.Count() == 2)
+            {
+                if (x.ElementAt(0).Count() == 2 || x.ElementAt(0).Count() == 3)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool isThreeOfAKind(int[] sorted)
+        {
+            var x = sorted.GroupBy(o => o);
+
+            if (x.Count() == 3)
+            {
+                if (x.ElementAt(0).Count() == 3 || x.ElementAt(1).Count() == 3 || x.ElementAt(2).Count() == 3)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool isTwoPair(int[] sorted)
+        {
+            var x = sorted.GroupBy(o => o);
+
+            if (x.Count() == 3)
+            {
+                if (x.ElementAt(0).Count() == 2 && x.ElementAt(1).Count() == 2 
+                    ||
+                    x.ElementAt(1).Count() == 2 && x.ElementAt(2).Count() == 2
+                    ||
+                    x.ElementAt(2).Count() == 2 && x.ElementAt(0).Count() == 2 
+
+                    ){
+
+                        return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public bool IsPair(int[] sorted)
+        {
+            var x = sorted.GroupBy(o => o);
+
+            if (x.Count() == 4)
+            {
+                if (x.ElementAt(0).Count() == 2 || x.ElementAt(1).Count() == 2 || x.ElementAt(2).Count() == 2 || x.ElementAt(3).Count() == 2)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        private IEnumerable<string> Split(string str, int chunkSize)
+        {
+            return Enumerable.Range(0, str.Length / chunkSize)
+                .Select(i => str.Substring(i * chunkSize, chunkSize));
+        }
+    }
+}
