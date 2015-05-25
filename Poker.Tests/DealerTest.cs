35fde50f1283ca89c949b7a2f63cd5aed27caaa8
Doruk.Ulucay <doruk.ulucay@cronom.com>
2015-05-25 22:46:08 +0300

    dealer now deals player hands. also some refactoring

--- Poker.Tests/DealerTest.cs
+++ Poker.Tests/DealerTest.cs
@@ -9,11 +9,23 @@ namespace Poker.Tests
         [TestMethod]
         public void CalculateHands_GivenPairAndFourOfAKind_ExptectedWinnerIsSecondHand()
         {
-            Dealer D = new Dealer();
+            Dealer D = new Dealer(2);
             CalculatedHand Winner = D.CalculateHands(new string[] { "H2H7D6SJCJ", "HKHJDKSKCK" });
 
             Assert.AreEqual(HandRanks.FourOfAKind, Winner.Rank);
 
+        }
+
+
+        [TestMethod]
+        public void DealPlayerHands_DealHandsForThreePlayers_ExpectThreeHands()
+        {
+            Dealer D = new Dealer(3);
+            
+            D.NewHand();
+            string[] Hands = D.DealPlayerHands();
+
+            Assert.AreEqual(3, Hands.Length);
         }
     }
 }
--- Poker/Dealer.cs
+++ Poker/Dealer.cs
@@ -8,6 +8,64 @@ namespace Poker
 {
     public class Dealer
     {
+
+        private int _playerNumber;
+
+        private string[] Deck;
+
+        public void NewHand()
+        {
+           Deck = NewDeck();
+        }
+
+        public Dealer(int PlayerNumber)
+        {
+            
+            _playerNumber = PlayerNumber;    
+        }
+
+        public string[] DealPlayerHands()
+        {
+            string[] PlayerHands = new string[_playerNumber];
+            int count = 0;
+
+            for (int i = 0; i < _playerNumber; i++)
+            {
+                
+                PlayerHands[i] = string.Format("{0}|{1}", Deck[count++], Deck[count++]);
+                
+            }
+
+
+            return PlayerHands;
+        }
+
+        public string[] NewDeck()
+        {
+            string[] NewDeck = new string[52];
+            int count = 0;
+
+            foreach (Poker.Suites item in Enum.GetValues(typeof(Poker.Suites)))
+            {
+                for (int i = 1; i < 14; i++)
+                {
+                    string Value = string.Empty;
+
+                    NewDeck[count] = string.Format("{0} of {1}", i, item.ToString());
+
+                    count++;
+
+                }
+            }
+
+            Random rnd = new Random();
+            string[] MyRandomArray = NewDeck.OrderBy(x => rnd.Next()).ToArray();
+
+            return MyRandomArray;
+        }
+
+
+
         public CalculatedHand CalculateHands(string[] Hands)
         {
             HandCalculator HC = new HandCalculator(false);
